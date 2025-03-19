ff818370caf80e8b442aa4cddc314aa3efac882c
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-03-19 22:23:41 +0100

    Remove dotenv dependency and update example environment variables for better compatibility

--- _includes/_data/settings.js
+++ _includes/_data/settings.js
@@ -1,9 +1,5 @@
-// import dotenv if you are not using github pages
-// import dotenv from "dotenv";
-// dotenv.config();
-
 export default {
-    siteName: process.env.SITE_NAME || "My Tech {log}",
-    siteLang: process.env.SITE_LANG || "en",
-    blogDescription: process.env.BLOG_DESCRIPTION || "A blog about web development, programming, and technology.",
+    siteName: "arnau.log",
+    siteLang: "es",
+    blogDescription: "",
 };
